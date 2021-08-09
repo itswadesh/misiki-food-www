8cabaaa8d8a04e94f0bb94a939c46949850033c0
Swadesh Behera <itswadesh@gmail.com>
2021-08-09 20:39:28 +0530

    add store to cart through actions_sass

--- store/cart.js
+++ store/cart.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 import sharedState from '~/shared/store/cart/state'
 import sharedGetters from '~/shared/store/cart/getters'
 import sharedMutation from '~/shared/store/cart/mutations'
-import sharedAction from '~/shared/store/cart/actions'
+import sharedAction from '~/shared/store/cart/actions_sass'
 Vue.use(Vuex)
 
 export const state = () => sharedState
