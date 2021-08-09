14edf6ea97aa2c87bd3257e5c503841173402af3
Pratap <pratapofficial1111@gmail.com>
2021-08-09 10:52:33 +0530

    side bar radio button fixed

--- shared/components/ui/Checkbox.vue
+++ shared/components/ui/Checkbox.vue
@@ -18,23 +18,27 @@
         border-gray-400
         cursor-pointer
         rounded-sm
-        focus:outline-0
-        focus:ring-0 focus:ring-offset-0
+        focus:outline-0 focus:ring-0 focus:ring-offset-0
       "
       :class="{ 'text-primary-500': color == 'primary' }"
       @click="toggle"
     />
+
     <!-- class="bg-gray-100 border-none rounded-sm text-primary-500 ring-gray-400 ring-1 focus:ring-1 focus:ring-gray-400 focus:ring-offset-0" -->
+
     <div
       v-if="docColor"
       data-colorhex="black"
       class="w-4 h-4 rounded-full shadow ms-1"
       :style="{ 'background-color': docColor }"
     ></div>
-    <div class="text-sm text-gray-800 ms-1">
+
+    <div class="text-sm text-gray-800 ms-2">
       <slot />
     </div>
-    <div v-if="count" class="text-xs text-gray-500">({{ count }})</div>
+
+    <div v-if="count" class="ms-2 text-xs text-gray-500">({{ count }})</div>
+
     <div class=""></div>
   </label>
 </template>
@@ -46,74 +50,89 @@ export default {
     prop: 'model',
     event: 'change',
   },
+
   props: {
     id: {
       type: String,
       required: false,
       default: null,
     },
+
     model: {
       type: String | Array,
       default: undefined,
     },
+
     docColor: {
       type: String,
       required: false,
       default: null,
     },
+
     color: {
       type: String,
       required: false,
       default: null,
     },
+
     circle: {
       type: Boolean,
       default: false,
       required: false,
     },
+
     checked: {
       type: Boolean,
       required: false,
     },
+
     value: {
       type: [String, Boolean],
       required: false,
       default: '',
     },
+
     count: {
       type: [String, Number],
       required: false,
       default: null,
     },
+
     name: {
       type: String,
       required: false,
       default: null,
     },
+
     required: {
       type: Boolean,
       required: false,
       default: false,
     },
+
     disabled: {
       type: Boolean,
       required: false,
       default: false,
     },
+
     size: {
       type: Number,
       required: false,
       default: 0,
     },
+
     fontSize: {
       type: Number,
       required: false,
       default: 0,
     },
   },
+
   data: () => ({
     uniqueId: '',
   }),
+
   computed: {
     checkboxState() {
       // if (this.model === undefined) return this.value;
@@ -121,17 +140,20 @@ export default {
       return this.model && this.value
     },
   },
+
   watch: {
     checked(v) {
       if (v !== this.checkboxState) this.toggle()
     },
   },
+
   mounted() {
     this.genId()
     if (this.checked && !this.checkboxState) {
       this.toggle()
     }
   },
+
   methods: {
     toggle() {
       if (this.disabled) return
--- shared/components/ui/Radio.vue
+++ shared/components/ui/Radio.vue
@@ -1,6 +1,7 @@
 <template>
   <label class="cursor-pointer flex items-center text-gray-800" :class="color">
     <!-- class="w-4 h-4 my-auto bg-gray-100 text-primary-500 focus:ring-1 focus:ring-yellow-600 focus:ring-offset-0" -->
+
     <input
       type="radio"
       :checked="checked"
@@ -15,30 +16,37 @@
       "
       @change="updateInput"
     />
-    <div class="ml-2 font-semibold">
+
+    <div class="text-sm text-gray-800 ms-2">
       <slot />
     </div>
   </label>
 </template>
+
 <script>
 export default {
   model: {
     prop: 'modelValue',
     event: 'change',
   },
+
   props: {
     value: {
       type: [String, Object, Number],
       default: null,
     },
+
     modelValue: { type: String, default: '' },
+
     color: { type: String, default: 'primary' },
   },
+
   computed: {
     checked() {
       return this.modelValue === this.value
     },
   },
+
   methods: {
     updateInput() {
       this.$emit('change', this.value)
