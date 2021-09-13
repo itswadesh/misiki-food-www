e4fce18061b230bc731a4e38d5220cc55c1f6b18
Swadesh Behera <itswadesh@gmail.com>
2021-09-13 08:30:05 +0530

    default , index put back

--- layouts/default.vue
+++ layouts/default.vue
@@ -40,7 +40,8 @@
       </div>
     </div>
 
-    <IslandFooter />
+    <WhiteFooter class="hidden sm:block" />
+
     <OtpLogin v-if="showLoginModal" @showLogin="showLogin" />
     <Modal :show="store.closed" title="Store is not accepting orders now">
       {{ store.closedMessage || 'Please visit back later' }}
@@ -53,16 +54,8 @@ import Nav from '~/components/Home/Nav.vue'
 import WhiteFooter from '~/components/Island/WhiteFooter.vue'
 import { Modal } from '~/shared/components/ui'
 import PrimaryButtonRounded from '~/components/ui/PrimaryButtonRounded.vue'
-import IslandFooter from '~/components/Island/IslandFooter.vue'
 export default {
-  components: {
-    Nav,
-    OtpLogin,
-    WhiteFooter,
-    Modal,
-    PrimaryButtonRounded,
-    IslandFooter,
-  },
+  components: { Nav, OtpLogin, WhiteFooter, Modal, PrimaryButtonRounded },
   data() {
     return {
       showLoginModal: false,
