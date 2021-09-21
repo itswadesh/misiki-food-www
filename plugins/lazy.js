9b16b18b1e82a2b27a8ac661470598ad5b54f7e0
Swadesh Behera <itswadesh@gmail.com>
2021-09-21 22:14:21 +0530

    fix: v-lazy

--- plugins/lazy.js
+++ plugins/lazy.js
@@ -19,10 +19,21 @@ export default ({ store, app }) => {
         const S3_URL = store.state.settings && store.state.settings.S3_URL
         let src = listener.src
         if (src) {
-          const contains = src.includes(S3_URL)
-          if (CDN_URL && S3_URL && contains) src = src.replace(S3_URL, CDN_URL)
+          let loadingSrc = src
+          // if (CDN_URL && S3_URL) src = src.replace(S3_URL, CDN_URL)
+          try {
+            if (CDN_URL) {
+              const url = new URL(src)
+              src = src.replace(url.origin, CDN_URL)
+              loadingSrc = CDN_URL + url.pathname + '?tr=w-3,h-2'
+            }
+          } catch (e) {
+            src = CDN_URL + src // When image comes from frontend e.g. /socialmedia/linkedin.png
+            loadingSrc = src + '?tr=w-3,h-2'
+          }
+          // const contains = src.includes(S3_URL)
           listener.el.setAttribute('lazy-progressive', 'true')
-          listener.loading = src + '?tr=w-3,h-2'
+          listener.loading = loadingSrc
           listener.src = src
         }
       },
