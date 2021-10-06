89d064abf1f66650938651c24271b0f7b5c980f7
Pratap <pratapofficial1111@gmail.com>
2021-10-06 19:25:44 +0530

    delete account added to profile

--- shared/components/ui/Textarea.vue
+++ shared/components/ui/Textarea.vue
@@ -1,48 +1,31 @@
 <template>
-  <div class="group">
-    <center>
-      <div class="floating-form">
-        <div
-          class="
-            floating-label
-            border-l border-t border-r border-gray-300
-            rounded-md
-          "
-        >
-          <textarea
-            class="
-              pt-4
-              ps-2.5
-              p-2
-              w-full
-              bg-gray-50
-              rounded-md
-              floating-input
-              hover:bg-white
-              focus:outline-none
-            "
-            placeholder=" "
-            v-bind="$attrs"
-            :value="value"
-            :type="type"
-            :aria-label="label"
-            :rows="rows"
-            @input="$emit('input', $event.target.value)"
-            @blur="$emit('blur', $event.target.value)"
-            @change="$emit('change', $event.target.value)"
-          />
-          <span class="highlight"></span>
-          <label>{{ label }}</label>
-        </div>
-      </div>
-    </center>
-  </div>
+  <textarea
+    class="
+      w-full
+      p-2
+      text-sm
+      placeholder-gray-400
+      border border-gray-400
+      rounded-md
+      bg-gray-50
+      hover:bg-white hover:border-primary-500
+      focus:outline-none
+    "
+    placeholder=" "
+    v-bind="$attrs"
+    :value="value"
+    :type="type"
+    :aria-label="label"
+    :rows="rows"
+    @input="$emit('input', $event.target.value)"
+    @blur="$emit('blur', $event.target.value)"
+    @change="$emit('change', $event.target.value)"
+  />
 </template>
 
 <script>
 export default {
   props: {
-    label: { type: String, default: null },
     type: { type: String, default: 'text' },
     value: { type: String, default: null },
     rows: { type: Number, default: 4 },
@@ -50,54 +33,4 @@ export default {
 }
 </script>
 
-<style scoped>
-textarea {
-  overflow: auto;
-}
-.floating-label {
-  position: relative;
-  margin-bottom: 20px;
-}
-.floating-input {
-  display: block;
-  width: 100%;
-  border: none;
-  overflow: auto;
-}
-
-.floating-input {
-  border-bottom: 2px solid rgba(209, 213, 219, 1);
-}
-
-.floating-input:focus {
-  outline: none;
-  border-bottom: 2px solid rgba(0, 0, 255, 0.555);
-}
-
-label {
-  font-size: 1rem;
-  font-weight: normal;
-  position: absolute;
-  pointer-events: none;
-  left: 8px;
-  top: 11px;
-  transition: 0.2s ease all;
-  -moz-transition: 0.2s ease all;
-  -webkit-transition: 0.2s ease all;
-  @apply text-gray-500;
-}
-
-.floating-input:focus ~ label,
-.floating-input:not(:placeholder-shown) ~ label {
-  top: -2px;
-  font-size: 0.875rem;
-  @apply text-primary-500;
-}
-
-.floating-select:focus ~ label,
-.floating-select:not([value='']):valid ~ label {
-  top: -18px;
-  font-size: 14px;
-  color: #fb6340;
-}
-</style>
+<style scoped></style>
