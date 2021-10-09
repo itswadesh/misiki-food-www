8b59252a8ba38a30216db8e7ef430c331933560a
Pratap <pratapofficial1111@gmail.com>
2021-10-09 12:25:53 +0530

    /my and home page design improved

--- components/User/Dashboard/ReviewAndRating.vue
+++ components/User/Dashboard/ReviewAndRating.vue
@@ -1,96 +1,92 @@
 <template>
   <div class="max-w-full min-h-screen mx-auto text-gray-800">
-    <div class="bg-white rounded-md shadow-md border">
+    <div
+      v-if="reviews && reviews.count > 0"
+      class="
+        bg-white
+        rounded-md
+        shadow-md
+        border
+        p-3
+        sm:p-5
+        flex flex-row
+        justify-between
+      "
+    >
+      <span class="text-lg font-semibold">My Reviews({{ reviews.count }})</span>
       <div
-        v-if="reviews && reviews.count > 0"
-        class="p-3 sm:p-5 flex flex-row justify-between"
+        v-for="r in reviews.data"
+        :key="r.id"
+        class="w-full p-1 pb-4 border-b"
       >
-        <span class="text-lg font-semibold"
-          >My Reviews({{ reviews.count }})</span
-        >
-        <div
-          v-for="r in reviews.data"
-          :key="r.id"
-          class="w-full p-1 pb-4 border-b"
-        >
-          <div class="flex flex-col w-full mt-3 sm:flex-row">
-            <div v-if="r.pid" class="mx-auto h-36 sm:pe-5 sm:w-40">
-              <img
-                v-lazy="`${r.pid.img}?tr=w-160,h-144,fo-auto`"
-                alt="mobile"
-                class="object-contain w-full h-full"
-              />
-            </div>
-            <div
-              class="
-                relative
-                flex flex-col
-                h-auto
-                mt-4
-                sm:mt-0
-                w-full
-                sm:flex-1
-              "
-            >
-              <span class="text-gray-700">{{ r.message }}</span>
-              <div v-if="r.rating" class="flex flex-row my-2">
-                <span
-                  style="padding: 2px"
-                  class="
-                    w-8
-                    h-5
-                    my-auto
-                    text-xs text-center text-white
-                    rounded
-                    me-3
-                    py-auto
-                    bg-primary-500
-                  "
-                >
-                  {{ r.rating }} *
-                </span>
-                <span class="text-sm font-semibold text-normal">
-                  <p v-if="r.rating == 1" class="text-red-600">
-                    Very Disappointed
-                  </p>
-                  <p v-if="r.rating == 2" class="text-red-400">
-                    Slightly Disappointed
-                  </p>
-                  <p v-if="r.rating == 3" class="text-accent-500">Good</p>
-                  <p v-if="r.rating == 4" class="text-secondary-200">
-                    Very Good
-                  </p>
-                  <p v-if="r.rating == 5" class="text-[#c30b4e]">Awesome</p>
-                </span>
-              </div>
-              <span class="mb-2 text-sm">{{ r.message }}</span>
-
-              <div
+        <div class="flex flex-col w-full mt-3 sm:flex-row">
+          <div v-if="r.pid" class="mx-auto h-36 sm:pe-5 sm:w-40">
+            <img
+              v-lazy="`${r.pid.img}?tr=w-160,h-144,fo-auto`"
+              alt="mobile"
+              class="object-contain w-full h-full"
+            />
+          </div>
+          <div
+            class="relative flex flex-col h-auto mt-4 sm:mt-0 w-full sm:flex-1"
+          >
+            <span class="text-gray-700">{{ r.message }}</span>
+            <div v-if="r.rating" class="flex flex-row my-2">
+              <span
+                style="padding: 2px"
                 class="
-                  flex flex-col
-                  w-full
-                  mb-2
-                  text-sm text-gray-700
-                  lg:flex-row
+                  w-8
+                  h-5
+                  my-auto
+                  text-xs text-center text-white
+                  rounded
+                  me-3
+                  py-auto
+                  bg-primary-500
                 "
               >
-                <div v-if="r.user" class="flex flex-row">
-                  <h6>{{ r.user.firstName }} {{ r.user.lastName }}</h6>
-                  <svg
-                    class="w-4 mx-2 my-auto text-secondary-200"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                  <h6>Certified Buyer @ {{ r.createdAt | date }}</h6>
-                </div>
-                <!-- <span class="lg:ms-64">
+                {{ r.rating }} *
+              </span>
+              <span class="text-sm font-semibold text-normal">
+                <p v-if="r.rating == 1" class="text-red-600">
+                  Very Disappointed
+                </p>
+                <p v-if="r.rating == 2" class="text-red-400">
+                  Slightly Disappointed
+                </p>
+                <p v-if="r.rating == 3" class="text-accent-500">Good</p>
+                <p v-if="r.rating == 4" class="text-secondary-200">Very Good</p>
+                <p v-if="r.rating == 5" class="text-[#c30b4e]">Awesome</p>
+              </span>
+            </div>
+            <span class="mb-2 text-sm">{{ r.message }}</span>
+
+            <div
+              class="
+                flex flex-col
+                w-full
+                mb-2
+                text-sm text-gray-700
+                lg:flex-row
+              "
+            >
+              <div v-if="r.user" class="flex flex-row">
+                <h6>{{ r.user.firstName }} {{ r.user.lastName }}</h6>
+                <svg
+                  class="w-4 mx-2 my-auto text-secondary-200"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <h6>Certified Buyer @ {{ r.createdAt | date }}</h6>
+              </div>
+              <!-- <span class="lg:ms-64">
                   <h4 class="flex flex-row">
                     <h5 class="flex flex-row me-4">
                       <thumbs-up-icon size="1x" class="my-auto me-2 custom-class"></thumbs-up-icon>
@@ -102,10 +98,10 @@
                     </h5>
                   </h4>
                 </span> -->
-              </div>
             </div>
           </div>
-          <!-- <div
+        </div>
+        <!-- <div
               class="flex flex-row justify-end mt-4 text-sm text-secondary-500"
             >
               <button class="font-semibold cursor-pointer focus:outline-none">
@@ -120,32 +116,27 @@
                 Share
               </button>
             </div> -->
-        </div>
       </div>
+    </div>
 
-      <div
-        v-else
+    <div v-else class="h-screen w-full">
+      <!-- v-lazy="`/img/review/review.png?tr=h-384,fo-auto`" -->
+      <img
+        src="/img/review/review.png"
+        alt="🚀"
+        class="object-contain object-top height w-full mx-auto"
+      />
+
+      <h2
         class="
-          h-screen
-          sm:h-auto
-          flex flex-col
-          items-center
-          justify-center
-          w-full
-          p-5
-          sm:p-10
+          text-center text-lg
+          sm:text-xl
+          font-medium font-serif
+          tracking-wide
         "
       >
-        <img
-          v-lazy="`/img/review/review.jpg?tr=h-384,fo-auto`"
-          alt="🚀"
-          class="object-contain h-60 sm:h-96 mb-5 sm:mb-10"
-        />
-
-        <h2 class="text-lg sm:text-xl font-medium font-serif tracking-wide">
-          No Reviews Yet !!
-        </h2>
-      </div>
+        No Reviews Yet !!
+      </h2>
     </div>
   </div>
 </template>
@@ -181,3 +172,16 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+@media (max-width: 768px) {
+  .height {
+    height: 55%;
+  }
+}
+@media (min-width: 768px) {
+  .height {
+    height: 65%;
+  }
+}
+</style>
